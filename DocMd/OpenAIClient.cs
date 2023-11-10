3f0f54c5b25170a730a08f34e8d72bbd7e633dec
justinprogdev <106097398+justinprogdev@users.noreply.github.com>
2023-11-09 23:29:23 -0500

    Fix namespace of http client

--- DocMd/OpenAIClient.cs
+++ DocMd/OpenAIClient.cs
@@ -7,47 +7,51 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-public class OpenAIClient
+namespace DocMd
 {
-    private readonly HttpClient _httpClient;
-    private readonly string _apiKey;
-    private readonly string _apiUrl = "https://api.openai.com/v1/chat/completions";
 
-    public OpenAIClient(string apiKey)
+    public class OpenAIClient
     {
-        _httpClient = new HttpClient();
-        _apiKey = apiKey;
-    }
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+        private readonly string _apiUrl = "https://api.openai.com/v1/chat/completions";
 
-    public async Task<string> CallOpenAiApiAsync(List<SourceFileInfo> sourceFileInfoCollection)
-    {
-
-        var promptMessage = new PromptMessage()
+        public OpenAIClient(string apiKey)
         {
-            SystemInstructions = MessagingConstants.SystemInstructions,
-            Code = sourceFileInfoCollection
-        };
+            _httpClient = new HttpClient();
+            _apiKey = apiKey;
+        }
 
-        var request = new
+        public async Task<string> CallOpenAiApiAsync(List<SourceFileInfo> sourceFileInfoCollection)
         {
-            model = "gpt-4",
-            messages = new[] { new Message { Role = "system", Content = JsonSerializer.Serialize(promptMessage)} },
-            max_tokens = 500,
-            temperature = 0.5,
-        };
 
-        // "max_tokens": 512,
-        //"top_p": 1,
-        //"temperature": 0.5,
-        //"frequency_penalty": 0,
-        //"presence_penalty": 0
+            var promptMessage = new PromptMessage()
+            {
+                SystemInstructions = MessagingConstants.SystemInstructions,
+                Code = sourceFileInfoCollection
+            };
 
-        var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
-        _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiKey);
+            var request = new
+            {
+                model = "gpt-4",
+                messages = new[] { new Message { Role = "system", Content = JsonSerializer.Serialize(promptMessage) } },
+                max_tokens = 500,
+                temperature = 0.5,
+            };
 
-        var response = await _httpClient.PostAsync(_apiUrl, content);
+            // "max_tokens": 512,
+            //"top_p": 1,
+            //"temperature": 0.5,
+            //"frequency_penalty": 0,
+            //"presence_penalty": 0
 
-        string resultContent = await response.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<ChatCompletionResponseModel>(resultContent)?.choices[0].message.content;
+            var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _apiKey);
+
+            var response = await _httpClient.PostAsync(_apiUrl, content);
+
+            string resultContent = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<ChatCompletionResponseModel>(resultContent)?.choices[0].message.content;
+        }
     }
 }
